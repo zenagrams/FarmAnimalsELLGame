1d8c27705ed9e58798c3a5feeb9be77817b1abc6
Zenaida <110210132+zenagrams@users.noreply.github.com>
2023-10-12 23:36:35 -0500

    Mapped keys to sound ids

---
+++ main.js
@@ -0,0 +1,11 @@
+let sounds ={
+    87 : "sound1",
+    65 : "sound2",
+    83 : "sound3",
+    68 : "sound4",
+    81 : "sound5",
+    69 : "sound6",
+    90: "sound7",
+    88 : "sound8",
+    67 : "sound9"
+}
